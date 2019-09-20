408336db3336e246def2c303fe849b9cebcb2ecb
jsmariegaard <jem@dhigroup.com>
2019-09-20 12:55:30 +0200

    added more tools

--- DfsUtilsRunner/Program.cs
+++ DfsUtilsRunner/Program.cs
@@ -58,14 +58,24 @@ namespace DHI.DFS.Utilities.Runner
 
         private static void _RunAddConstantTool(string[] args)
         {
-            throw new NotImplementedException();
+            if (args.Count() < 4)
+            {
+                Console.WriteLine(">DfsUtils AddConstant infile.dfsu 7 outfile.dfsu");
+                throw new ArgumentException("AddConstant needs 3 arguments");
+            }
+            var infile = args[1];
+            var cnst = Convert.ToDouble(args[2]);
+            var outfile = args[3];
+
+            var constantAdder = new DfsAddConstant();
+            constantAdder.Run(infile, cnst, outfile);
         }
 
         private static void _RunSumTool(string[] args)
         {
             if (args.Count() < 4)
             {
-                Console.WriteLine(">DfsUtils Sum infile.dfsu 0.9 outfile.dfsu");
+                Console.WriteLine(">DfsUtils Sum file1.dfsu file2.dfsu outfile.dfsu");
                 throw new ArgumentException("Sum needs 3 arguments");
             }
             var file1 = args[1];
@@ -78,12 +88,40 @@ namespace DHI.DFS.Utilities.Runner
 
         private static void _RunDiffTool(string[] args)
         {
-            throw new NotImplementedException();
+            if (args.Count() < 4)
+            {
+                Console.WriteLine(">DfsUtils Diff infile.dfsu 0.9 outfile.dfsu");
+                throw new ArgumentException("Diff needs 3 arguments");
+            }
+            var file1 = args[1];
+            var file2 = args[2];
+            var outfile = args[3];
+
+            var differ = new DfsDiff();
+            differ.Run(file1, file2, outfile);
         }
 
         private static void _RunExtractStepsTool(string[] args)
         {
-            throw new NotImplementedException();
+            if (args.Count() < 5)
+            {
+                Console.WriteLine(">DfsUtils ExtractSteps infile.dfsu outfile.dfsu start end [stride]");
+                Console.WriteLine(">DfsUtils ExtractSteps infile.dfsu outfile.dfsu 10 -1 2");
+                throw new ArgumentException("ExtractSteps needs 4 or 5 arguments");
+            }
+            var infile = args[1];            
+            var outfile = args[2];
+            var startstep = Convert.ToInt32(args[3]);
+            var endstep = Convert.ToInt32(args[4]);
+            if (args.Count() == 6)
+            {
+                var stride = Convert.ToInt32(args[5]);
+                DfsTimeStepsExtractor.Extract(infile, outfile, startstep, endstep, stride);
+            }
+            else
+            {
+                DfsTimeStepsExtractor.Extract(infile, outfile, startstep, endstep);
+            }
         }
 
         private static void _RunFlattenTool(string[] args)
