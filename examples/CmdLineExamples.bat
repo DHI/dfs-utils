7faa1c2ddfd485d5c571cde08d0a43089d31ddfa
Jesper Sandvig Mariegaard <34088801+jsmariegaard@users.noreply.github.com>
2019-10-22 11:54:29 +0200

    improved examples

--- examples/CmdLineExamples.bat
+++ examples/CmdLineExamples.bat
@@ -1,17 +1,19 @@
 set tool="..\DfsUtilsRunner\bin\Release\DfsUtils.exe"
 
 REM example 1
-%tool% ExtractSteps .\data\TS1.dfs0 .\output\TS1_ks.dfs0 0 3
-
-REM example 3
-%tool% Diff .\data\TS1.dfs0 .\data\TS2.dfs0 .\output\TS1_minus_TS2.dfs0
-
-REM example 4
-%tool% Sum .\data\TS1.dfs0 .\data\TS2.dfs0 .\output\TS1_plus_TS2.dfs0
-
-
-REM example 6
 %tool% Scale .\data\TS1.dfs0 2.0 .\output\TS1_scale2.dfs0
 
-REM example 7
+REM example 2
 %tool% AddConstant .\data\TS1.dfs0 2.0 .\output\TS1_plus2.dfs0
+
+REM example 3
+%tool% Sum .\data\TS1.dfs0 .\data\TS2.dfs0 .\output\TS1_plus_TS2.dfs0
+
+REM example 4
+%tool% Diff .\data\TS1.dfs0 .\data\TS2.dfs0 .\output\TS1_minus_TS2.dfs0
+
+REM example 5
+%tool% ExtractSteps .\data\TS1.dfs0 .\output\TS1_ks.dfs0 0 3
+
+REM example 6
+%tool% TimeAverage .\data\TS1.dfs0 .\output\TS1_avg.dfs0
--- examples/MatlabExamplesDfsUtils.m
+++ examples/MatlabExamplesDfsUtils.m
@@ -1,47 +1,52 @@
 %% load assembly
 NET.addAssembly([cd,'\..\DfsUtilsLib\bin\Release\DfsUtilsLib.dll']);
-import DHI.DFS.Utilities.*;  % DfsAdd, DfsDiff, DfsFactor, DfsKitchenSink
+import DHI.DFS.Utilities.*;  % DfsAdd, DfsDiff, DfsScale, ...
 
-%% example 1 
-file_in = 'data\TS1.dfs0';
-file_out = 'output\TS1_ks.dfs0';
-dfsks = DfsKitchenSink(file_in, file_out);
-dfsks.ExtractTimeSteps(0,3);
+%% example 1
+multi = DfsScale();
+filein  = 'data\TS1.dfs0';
+fileout = 'output\scale2.dfs0';
+multi.Run(filein, 2.0, fileout);
 
-%% example 2 (exact same as example 1, but different syntax)
-file_out = 'output\TS1_extract.dfs0';
-DfsTimeStepsExtractor.Extract(file_in, file_out, 0,3);
+%% example 2
+adder = DfsAddConstant();
+filein  = 'data\TS1.dfs0';
+fileout = 'output\add_constant.dfs0';
+adder.Run(filein, 2.0, fileout);
 
 %% example 3
+summer = DfsAdd();
+file1  = 'data\TS1.dfs0';
+file2  = 'data\TS2.dfs0';
+fileout = 'output\add.dfs0';
+summer.Run(file1, file2, fileout);
+
+%% example 3b
+summer = DfsAdd();
+file1  = 'data\TS1.dfs0';
+file2  = 'data\TS2.dfs0';
+fileout = 'output\add_90_10.dfs0';
+summer.Run(file1, file2, .9, .1, fileout);
+
+%% example 4
 differ = DfsDiff();
 file1  = 'data\wl_bc1.dfs1';
 file2  = 'data\wl_bc2.dfs1';
 fileout = 'output\diff.dfs1';
-differ.Run(file1,file2,fileout);
+differ.Run(file1, file2, fileout);
 
-%% example 4
-combiner = DfsAdd();
-file1  = 'data\TS1.dfs0';
-file2  = 'data\TS2.dfs0';
-fileout = 'output\add.dfs0';
-combiner.Run(file1,file2,fileout);
+%% example 5: extract time steps 
+file_in = 'data\TS1.dfs0';
+file_out = 'output\TS1_ks.dfs0';
+dfsks = DfsKitchenSink(file_in, file_out);
+dfsks.ExtractTimeSteps(0, 3);
 
-%% example 5
-combiner = DfsAdd();
-file1  = 'data\TS1.dfs0';
-file2  = 'data\TS2.dfs0';
-fileout = 'output\add_90_10.dfs0';
-combiner.Run(file1,file2,.9,.1,fileout);
+%% example 5b (exact same as example 5, but different syntax)
+file_out = 'output\TS1_extract.dfs0';
+DfsTimeStepsExtractor.Extract(file_in, file_out, 0,3);
 
-%% example 6
-multi = DfsScale();
-filein  = 'data\TS1.dfs0';
-fileout = 'output\scale2.dfs0';
-multi.Run(filein,2.0,fileout);
-
-%% example 7
-adder = DfsAddConstant();
-filein  = 'data\TS1.dfs0';
-fileout = 'output\add_constant.dfs0';
-adder.Run(filein,2.0,fileout);
-
+%% example 6: make time averaged file
+file_in = 'data\TS1.dfs0';
+file_out = 'output\TS1_avg.dfs0';
+flattener = DfsFlatten();
+flattener.Run(file_in, file_out);
